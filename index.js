6426d48117fb3de47bc022ebc7a31053e9b245f6
Ahiram Cordova (AyJay) <ahiramcordova@gmail.com>
2020-09-10 18:26:22 -0700

    Added remainders

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 let decimal = prompt("What is your decimal?");
 
-let hexidecimal = (decimal / 16);
+if(remainder1 == 10) {
+    remainder1 = "A"
+};
+if(remainder1 == 11) {
+    remainder1 = "B"
+};
+if(remainder1 == 12) {
+    remainder1 = "C"
+};
+if(remainder1 == 13) {
+    remainder = "D"
+};
+if(remainder1 == 14) {
+    remainder1 = "E"
+};
+if(remainder1 == 15) {
+    remainder = "F"
+};
+
 
 console.log(`You have ${hexidecimal}`);
